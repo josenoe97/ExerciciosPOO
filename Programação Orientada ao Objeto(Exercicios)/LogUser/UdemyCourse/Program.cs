0c246670b3e4841261af359038e00f5355914f68
José Edson Jr <edsonjunior9701@outlook.com>
2023-08-03 13:38:49 -0300

    Aplicacao prática de Equals e HashSet, para evitar repeticoes de strings

---
+++ Programação Orientada ao Objeto(Exercicios)/LogUser/UdemyCourse/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyCourse.Entities;
+
+namespace UdemyCourse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<LogRecord> set = new HashSet<LogRecord>();
+
+            Console.Write("Enter file full path: ");
+            string sourcePath = Console.ReadLine();
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(sourcePath))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(' ');
+                        string name = line[0];
+                        DateTime instante = DateTime.Parse(line[1]);
+                        set.Add(new LogRecord {UserName = name, Instant = instante});
+                        Console.WriteLine(line[0] + " " + line[1]);
+                    }
+                    Console.WriteLine();
+                    Console.WriteLine("Total users: " + set.Count);
+
+                }
+            }
+            catch(IOException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
